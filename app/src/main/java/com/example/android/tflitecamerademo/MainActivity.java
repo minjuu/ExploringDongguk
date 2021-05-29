7935cfe12c4dfc11df68130d506a4974bef97590
minjuu <minju9809@naver.com>
2021-05-29 22:12:20 +0900

    초기화면

---
+++ app/src/main/java/com/example/android/tflitecamerademo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.android.tflitecamerademo;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import com.example.android.tflitecamerademo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Dialog dilaog01; // 커스텀 다이얼로그
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_camera2_basic);
+
+        dilaog01 = new Dialog(MainActivity.this);       // Dialog 초기화
+        dilaog01.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+        dilaog01.setContentView(R.layout.dialog01);             // xml 레이아웃 파일과 연결
+
+        // 버튼: 커스텀 다이얼로그 띄우기
+        findViewById(R.id.showBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog01(); // 아래 showDialog01() 함수 호출
+            }
+        });
+    }
+
+    // dialog01을 디자인하는 함수
+    public void showDialog01(){
+        dilaog01.show(); // 다이얼로그 띄우기
+
+        /* 이 함수 안에 원하는 디자인과 기능을 구현하면 된다. */
+
+        // 위젯 연결 방식은 각자 취향대로~
+        // '아래 아니오 버튼'처럼 일반적인 방법대로 연결하면 재사용에 용이하고,
+        // '아래 네 버튼'처럼 바로 연결하면 일회성으로 사용하기 편함.
+        // *주의할 점: findViewById()를 쓸 때는 -> 앞에 반드시 다이얼로그 이름을 붙여야 한다.
+
+        // 아니오 버튼
+        Button noBtn = dilaog01.findViewById(R.id.noBtn);
+        noBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 원하는 기능 구현
+                dilaog01.dismiss(); // 다이얼로그 닫기
+            }
+        });
+        // 네 버튼
+        dilaog01.findViewById(R.id.yesBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 원하는 기능 구현
+                finish();           // 앱 종료
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/example/android/tflitecamerademo/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.tflitecamerademo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends Activity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(4000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, CameraActivity.class));
+        finish();
+    }
+}
