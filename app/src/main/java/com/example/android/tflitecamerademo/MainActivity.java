b51a5b8234020387b751ed41ad3852cef32555b8
minjuu <minju9809@naver.com>
2021-06-11 19:19:55 +0900

    카카오맵 api 성공22

--- app/src/main/java/com/example/android/tflitecamerademo/MainActivity.java
+++ app/src/main/java/com/example/android/tflitecamerademo/MainActivity.java
@@ -1,33 +1,23 @@
 package com.example.android.tflitecamerademo;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.ViewGroup;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
 import net.daum.mf.map.api.MapView;
 
 
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.dgumap);
-//
-//        MapView mapView = new MapView(this);
-//
-//        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-//        mapViewContainer.addView(mapView);
-//        Log.d("test","ddddddd");
-//    }
-//
-//}
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.dgumap);
 
         MapView mapView = new MapView(this);
