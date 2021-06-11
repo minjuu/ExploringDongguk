fef00d1a7a9daed2f00079d824302d437c35cad2
minjuu <minju9809@naver.com>
2021-06-11 22:24:32 +0900

    마커 추가

--- app/src/main/java/com/example/android/tflitecamerademo/MainActivity.java
+++ app/src/main/java/com/example/android/tflitecamerademo/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
+import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
@@ -24,6 +25,20 @@ public class MainActivity extends Activity {
         MapView mapView = new MapView(this);
         // 중심점 변경
         mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.558436292058964, 127.00016774048937), true);
+
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.557770725591254, 127.00187371702079);//혜화관
+        MapPOIItem customMarker = new MapPOIItem();
+
+        customMarker.setItemName("Hyehwa");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(MARKER_POINT);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.mapicon); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+
+        mapView.addPOIItem(customMarker);
+
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
         Log.d("test","ddddddd");
